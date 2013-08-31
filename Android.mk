98816ad5adfccbcb8575acdd6cf09243ac0b645d
Andrew Dodd <atd7@cornell.edu>
2013-08-31 12:53:41 -0400

    Add audio firmware symlinks
    Change-Id: I5ea375a05d0cdf9b8f783382911e7652318a0e15

--- Android.mk
+++ Android.mk
@@ -39,3 +39,8 @@ endif
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9310; \
+    ln -sf /data/misc/audio/wcd9310_anc.bin \
+    $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
+    ln -sf /data/misc/audio/mbhc.bin \
+    $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin)
