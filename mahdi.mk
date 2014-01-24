de6c7ecbe52157c462291c855676d3b2ca580047
flappjaxxx <flappjaxxx@gmail.com>
2014-01-23 21:49:40 -0600

    Initial mahdi commits

--- mahdi.mk
+++ mahdi.mk
@@ -1,22 +1,22 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mahdi/configs/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mahdi/configs/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mahdi/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/oppo/find5/full_find5.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := find5
-PRODUCT_NAME := cm_find5
+PRODUCT_NAME := mahdi_find5
 PRODUCT_BRAND := Oppo
 PRODUCT_MODEL := Find 5
 PRODUCT_MANUFACTURER := Oppo
